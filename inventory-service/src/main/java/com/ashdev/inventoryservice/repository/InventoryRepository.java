2d3f0d11f412cb4961a7fa1739d887f4bcb1b752
Aayush Patel <ayushp264@gmail.com>
2023-06-10 21:23:53 +0530

    Add files via upload
    spring boot and microservices update

---
+++ inventory-service/src/main/java/com/ashdev/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.ashdev.inventoryservice.repository;
+
+import com.ashdev.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
